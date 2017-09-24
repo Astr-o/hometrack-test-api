6d7932c203e0df58c1c555574f0f9d991ad61984
Kroum Klutchkov <kroum.klutchkov@gmail.com>
2017-09-24 18:41:35 +1000

    adds express app implmentation

--- server.js
+++ server.js
@@ -1,13 +1,49 @@
-const config = require('./config').express;
-
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const config = require('./config').express;
+const propertyProcessor = require('./internal_modules/property-processor');
 
 const app = express();
 
-app.get('*', (req, res) => {
-    res.send('Hello World I am here !!!!')
-})
+app.use(bodyParser.json());
+app.use(morgan('combined'));
+
+app.post('/', (req, res, next) => {
+    try {
+        const reqPayload = req.body.payload;
+
+        const resPayload = propertyProcessor
+            .filterHtvCompleteProperties(reqPayload)
+            .map(prop => {
+                return {
+                    concataddress: propertyProcessor.concatAddress(prop.address),
+                    type: prop.type,
+                    workflow: prop.workflow
+                };
+            });
+
+        res.send({ response: resPayload });
+
+    } catch (err) {
+        next(err);
+    }
+});
+
+// default error handler
+app.use((err, req, res, next) => {
+    console.error(err.message);
+    console.debug(err.stack);
+
+    // handle body parser parse error 
+    if (err.type === 'entity.parse.failed') {
+        res.status(400).send({ error: 'Could not decode request: JSON parsing failed' });
+    } else {
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
+});
 
 app.listen(config.port, () => {
-    console.log('server listening on ::' + config.port)
-})
+    console.log('server listening on ::' + config.port);
+});
