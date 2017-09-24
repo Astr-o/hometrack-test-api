90438f07ce89f74211a5d3982a61435a8a55a71c
Kroum Klutchkov <kroum.klutchkov@gmail.com>
2017-09-24 17:59:37 +1000

    Adds compeleted property-processor module with all tests passing

--- internal_modules/property-processor.js
+++ internal_modules/property-processor.js
@@ -2,10 +2,31 @@
  * Filter list of properties only to those where type is htv and workflow is completed.
  */
 module.exports.filterHtvCompleteProperties = function filterHtvCompleteProperties(propertyData) {
+    if (!Array.isArray(propertyData)) {
+        throw new Error('Validation Error: propertyData is not of Array type');
+    }
 
+    return propertyData.filter(prop => prop.type === 'htv' && prop.workflow === 'completed');
 }
 
 /**
  * Concatinate Address object to string
  */
-module.exports.concatAddress = function concatAddress(address) {}
+module.exports.concatAddress = function concatAddress(address) {
+    if (address === null || address === undefined || typeof address !== 'object') {
+        throw new Error('Validation Error: address is not of Object type');
+    }
+
+    // use array to order object fields, then removes nulls with filter, then uses join to create string
+    return [
+            address.unitNumber,
+            address.buildingNumber,
+            address.street,
+            address.suburb,
+            address.state,
+            address.postcode,
+        ]
+        .filter(i => i)
+        .join(' ');
+
+}
